03d5633387752f1ceb0090f76923403f7feef042
Laxman Bhusal <laxmanbhusal612@gmail.com>
2022-04-05 23:58:53 +0545

    Start of new Chapter

--- src/Components/AppBar/AppBarred.jsx
+++ src/Components/AppBar/AppBarred.jsx
@@ -9,7 +9,7 @@ export default function AppBarred({ isLoggedin }) {
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" style={{ flexGrow: 1 }}>
-                        <Link style={{ textDecoration: 'none', textAnchor: 'unset', color: "inherit" }} to={"/"}>   Bookstore(IMJC)</Link>
+                        <Link style={{ textDecoration: 'none', textAnchor: 'unset', color: "inherit" }} to={"/"}>   Book Exchange</Link>
                     </Typography>
                     {!isLoggedin ? <Link style={{ textDecoration: 'none', textAnchor: 'unset' }} to={'/login'}> <Button color='secondary' >Login</Button></Link> : <Link style={{ textDecoration: 'none', textAnchor: 'unset' }} to={'/profile'}> <Button color='secondary' >Profile</Button></Link>}
                 </Toolbar>
